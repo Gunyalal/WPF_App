fef07ece26ed3616f01110f61258460c827df13e
Gunyalal <theslenderhorrors@gmail.com>
2024-04-11 09:57:56 +0400

    Update CreateStructure.sql

--- CreateStructure.sql
+++ CreateStructure.sql
@@ -4,18 +4,18 @@ create table dbo.RDOGOVOR (
   update_time date
 );
 
-INSERT INTO master.dbo.RDOGOVOR
+INSERT INTO dbo.RDOGOVOR
 ( dog_date, update_time)
 VALUES('2024-01-01', '2024-04-10');
-INSERT INTO master.dbo.RDOGOVOR
+INSERT INTO dbo.RDOGOVOR
 (dog_date, update_time)
 VALUES('2020-02-01', '2024-02-15');
-INSERT INTO master.dbo.RDOGOVOR
+INSERT INTO dbo.RDOGOVOR
 (dog_date, update_time)
 VALUES('2006-05-03', '2006-05-03');
-INSERT INTO master.dbo.RDOGOVOR
+INSERT INTO dbo.RDOGOVOR
 (dog_date, update_time)
 VALUES('2004-05-03', '2006-05-03');
-INSERT INTO master.dbo.RDOGOVOR
+INSERT INTO dbo.RDOGOVOR
 (dog_date, update_time)
 VALUES('2000-01-01', '2003-01-01');
